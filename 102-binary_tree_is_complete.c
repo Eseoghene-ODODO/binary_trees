5081d7947b18e850b85116bd7aaf88fb5bbcc3a8
Eseoghene-ODODO <en.ododo@gmail.com>
2024-03-27 13:36:49 +0000

    binary tree project

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -13,9 +13,10 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	size_t size;
 
 	if (!tree)
+	{
 		return (0);
+	}
 	size = binary_tree_size(tree);
-
 	return (btic_helper(tree, 0, size));
 }
 
@@ -32,11 +33,13 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 int btic_helper(const binary_tree_t *tree, size_t index, size_t size)
 {
 	if (!tree)
+	{
 		return (1);
-
+	}
 	if (index >= size)
+	{
 		return (0);
-
+	}
 	return (btic_helper(tree->left, 2 * index + 1, size) &&
 		btic_helper(tree->right, 2 * index + 2, size));
 }
@@ -51,8 +54,9 @@ int btic_helper(const binary_tree_t *tree, size_t index, size_t size)
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
+	{
 		return (0);
-
+	}
 	return (binary_tree_size(tree->left) +
 		binary_tree_size(tree->right) + 1);
 }
